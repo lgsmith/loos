3c5ca8975765bd7b509479bf690b1125670dcb39
Louis Smith <louis_smith@urmc.rochester.edu>
2020-06-15 17:41:24 -0400

    added missing factor of tau to rigid_spectral_density, added N_A back to value of xi because I was observing roundoff errors. Relative NOEs will not be per-molar, but abs-vals will be.
    Also added ini file to target one replica for real-data tests.

--- Packages/User/noes-corr.cpp
+++ Packages/User/noes-corr.cpp
@@ -30,8 +30,8 @@
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
 #include <Eigen/LU>
-#include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 #include <string>
 #include <vector>
 #define EIGEN_USE_THREADS
@@ -56,7 +56,7 @@ public:
        "(Over)write time series to provided file name. If empty, then not written.")
       ("nthreads,n", po::value<int>(&threads)->default_value(1),
        "Number of threads to use for the calculation.")
-      ("gamma,g", po::value<double>(&gamma)->default_value(42.58),
+      ("gamma,g", po::value<double>(&gamma)->default_value(42.577478518),
       "Set the gyromagnetic ratio, in MHz/T.")
       ("field-strength,B", po::value<double>(&B)->default_value(14.1),
        "Set the value of the experimental field strength, in T.")
@@ -73,7 +73,7 @@ public:
       ("spectral-density,J", po::bool_switch(&spectral_density)->default_value(false),
        "If thrown, use spectral densities for relaxation matrix elts.")
       ("initial-magnetization,M", po::value<double>(&M)->default_value(1),
-       "Initial magnetization (M_0) at t=0. If 1 is used, All NOEs relative.");
+       "Initial magnetization (M_0) at t=0.");
       
   }
   // clang-format on
@@ -142,11 +142,15 @@ pt::ptree make_NOE_json(vector<MatrixXd> &intensities, vector<double> &times,
 }
 
 // rigid tumbling spectral density, assumes uniform correlation times
-inline vector<MatrixXd>
-rigid_spectral_density(MatrixXd &dist6, vector<double> &omega, const double tau, const double total_weight) {
+inline vector<MatrixXd> rigid_spectral_density(MatrixXd &dist6,
+                                               vector<double> &omega,
+                                               const double tau,
+                                               const double total_weight) {
   vector<MatrixXd> jvec;
+  // (1/4*pi) * r^(-6) * (2\tau_c/(1 + w^2 \tau_c^2 ))
   for (auto w : omega)
-    jvec.push_back(dist6 * 2 * tau / ((1 + w * w * tau * tau) * total_weight));
+    jvec.push_back(dist6 * 2.0 * tau * tau /
+                   ((1.0 + w * w * tau * tau) * total_weight * 4.0 * PI));
   return jvec;
 }
 
@@ -204,20 +208,22 @@ int main(int argc, char *argv[]) {
   const double mu0 =
       1.25663706212e4; // wikipedia, H/Angstrom (10^10 * value in H/m)
   const double gamma =
-      topts->gamma * 2 * PI * mhz2Hz;  // convert Gamma from mHz/T to Rad/s*T
-  const double hbar = 1.054571817e-34; // wikipedia, J*s
+      topts->gamma * 2.0 * PI * mhz2Hz; // convert Gamma from mHz/T to Rad/s*T
+  const double hbar = 1.054571817e-34;  // wikipedia, J*s
   const double N_A = 6.02214076e23;    // Wikipedia, Avogadro's Constant
-  // dipolar interaction constant, unit distance per Mole
-  const double dd = gamma * gamma * mu0 * hbar / (4 * PI);
-  const double dd2 = N_A * dd * dd * 5.0 / (PI * 16);
+  // dipolar interaction constant from Brueschweiler & Case, 1994 (eqs 33-35)
+  const double xi = N_A * gamma * gamma * hbar * mu0 / (4.0 * PI);
+  // this is equivalent to 0.5 * zeta from eqn 33 in B&C.
+  // The 0.5 comes from factoring out of spectral density prefactors so that
+  // they'd be whole numbers.
+  const double zeta = xi * xi * 2.0 * PI / 10.0;
   // Larmor frequency, in Hz
-  const double omega = gamma * topts->B * mhz2Hz;
-  vector<double> frqs{0.0, omega, omega * 2};
+  const double omega = gamma * topts->B;
   // filled with spectral densities at frqs frequencies, below.
+  vector<double> frqs{0.0, omega, omega * 2.0};
   vector<MatrixXd> J;
   // matrix to hold return values of calculation in forloop
   MatrixXd sample = MatrixXd::Zero(N, N);
-  MatrixXd R(N, N);
   if (topts->spectral_density) {
     // Magic circle oscillator precomputation:
     // Magic Circle oscillator for trackin samples like the above
@@ -236,8 +242,8 @@ int main(int argc, char *argv[]) {
     // compute spectral densities from DFT here.
     J = dft.spectral_density();
 
-  } else {         // use r^6 approx/averaging
-    double d2 = 0; // stores output of squared distance
+  } else {           // use r^6 approx/averaging
+    double d2 = 0.0; // stores output of squared distance
     for (auto f : mtopts->frameList()) {
       traj->readFrame(f);
       traj->updateGroupCoords(nuclei);
@@ -252,51 +258,45 @@ int main(int argc, char *argv[]) {
       wopts->weights->accumulate();
     }
     // use approximate formula for spectral densitiies here.
-    J = rigid_spectral_density(sample, frqs, topts->tau * ns2s, wopts->weights->totalWeight());
+    J = rigid_spectral_density(sample, frqs, topts->tau * ns2s,
+                               wopts->weights->totalWeight());
   }
   // following van gunsteren, compute spectral density based rho and sigma
-  MatrixXd rho((J[0] + (3 * J[1]) + (6 * J[2])).selfadjointView<Lower>());
-  R = (6 * J[2]) - J[0];
+  MatrixXd rho((J[0] + (3.0 * J[1]) + (6.0 * J[2])).selfadjointView<Lower>());
+  MatrixXd R(((6.0 * J[2]) - J[0]).selfadjointView<Lower>());
   R.diagonal(0) = rho.colwise().sum();
   cout << "this is R, but not in correct units:\n" << R << "\n";
-  R *= dd2;
+  // multiply by units here
+  R *= zeta;
   cout << "this is R:\n" << R << "\n";
   vector<MatrixXd> intensities;
   pt::ptree jsontree;
   jsontree.put("invocation", header);
   if (topts->isa) {
-    vector<double> mix{topts->m*ms2s};
+    vector<double> mix{topts->m * ms2s};
     intensities.push_back(move(R * mix[0]));
     jsontree = make_NOE_json(intensities, mix, nuclei);
   } else {
+    // SelfAdjointEigensovler only looks at the lower triangle, so it is not an
+    // issue that the upper triangle of the J-arithmetic above is zeros.
     SelfAdjointEigenSolver<MatrixXd> es(R);
-    R = R.selfadjointView<Lower>();
     MatrixXd eVecs = es.eigenvectors();
     cout << "eVecs:\n" << eVecs << endl;
     MatrixXd invEvecs = eVecs.inverse();
     cout << "invEvecs:\n" << invEvecs << endl;
-    cout << "R - eVecs*evals*invEvecs:\n" 
-         << R - (eVecs * es.eigenvalues().asDiagonal() * invEvecs) 
-         << endl;
-    if (topts->M == 1.0){
-      for (const auto time : topts->buildups){
-        cout << "time: " << time << "\n";
-        intensities.push_back(
-          eVecs * 
-          (es.eigenvalues() * (-time * ms2s)).array().exp().matrix().asDiagonal() *
-          invEvecs
-        );
-        cout << intensities.back() << endl;
-      }
-    } else {
-      for (const auto time : topts->buildups){
-        cout << "time: " << time << "\n";
-        intensities.push_back(
-          eVecs * 
-          (es.eigenvalues() * (-time * ms2s)).array().exp().matrix().asDiagonal() *
-          invEvecs * (topts->M * MatrixXd::Identity(N, N))
-        );
-      }
+    cout << "R - eVecs*evals*invEvecs:\n"
+         << R - (eVecs * es.eigenvalues().asDiagonal() * invEvecs) << endl;
+    if (topts->M != 1.0) {
+      invEvecs *= topts->M;
+    }
+    for (const auto time : topts->buildups) {
+      intensities.push_back(eVecs *
+                            (es.eigenvalues() * (-time * ms2s))
+                                .array()
+                                .exp()
+                                .matrix()
+                                .asDiagonal() *
+                            invEvecs);
     }
     jsontree = make_NOE_json(intensities, topts->buildups, nuclei);
     ComputationInfo es_info = es.info();
@@ -304,11 +304,13 @@ int main(int argc, char *argv[]) {
     if (es_info == Success)
       jsontree.put(comp_info_tag, "Eigendecomposition successful.");
     if (es_info == NumericalIssue)
-      jsontree.put(comp_info_tag, "Eigendecomposition ran into a numerical issue.");
+      jsontree.put(comp_info_tag,
+                   "Eigendecomposition ran into a numerical issue.");
     if (es_info == NoConvergence)
       jsontree.put(comp_info_tag, "Eigendecomposition did not converge.");
     if (es_info == InvalidInput)
-      jsontree.put(comp_info_tag, "Eigendecomposition was given invalid input.");
+      jsontree.put(comp_info_tag,
+                   "Eigendecomposition was given invalid input.");
   }
   pt::write_json(cout, jsontree);
 }
